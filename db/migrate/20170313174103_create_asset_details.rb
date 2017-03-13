6c99efabb16a1182a42563e9d09f411cce56c326
Walter Alves <walter.arruda.alves@gmail.com>
2017-03-13 16:27:10 -0300

    Change assets_details to asset_details
    Change table name to asset_details

--- db/migrate/20170313174103_create_asset_details.rb
+++ db/migrate/20170313174103_create_asset_details.rb
@@ -1,6 +1,6 @@
 class CreateAssetsDetails < ActiveRecord::Migration[5.0]
   def change
-    create_table :assets_details do |t|
+    create_table :asset_details do |t|
       t.text  :description
       t.text  :location
       t.text  :room
